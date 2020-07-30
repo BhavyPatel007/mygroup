90326d5d065be4889b2c4441b6fc0b1194ea48e1
BHAVY <bhavysoliya007@gmail.com>
2020-07-30 12:01:17 +0530

    create splash screen

---
+++ app/src/main/java/com/example/mygroup/splech_screen.java
@@ -0,0 +1,14 @@
+package com.example.mygroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splech_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splech_screen);
+    }
+}
