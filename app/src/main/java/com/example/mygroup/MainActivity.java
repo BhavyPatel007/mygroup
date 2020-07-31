2caf2955e038f85bfeae38156a395d179aeb0da1
BHAVY <bhavysoliya007@gmail.com>
2020-07-31 09:42:51 +0530

    create splash screen

--- app/src/main/java/com/example/mygroup/MainActivity.java
+++ app/src/main/java/com/example/mygroup/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.mygroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int splash_time=3000;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splech_screen);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, login_page.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            },splash_time);
+        }
 }
